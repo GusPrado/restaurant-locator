360e445463df7980f760c435b2dfd9eb18428d36
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-11-12 23:17:56 -0300

    Finish search field & add screen areas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 import logo from '../../assets/logo.svg';
 
-import { Container, Search } from './styles';
+import { Container, Search, Logo, Wrapper, Map } from './styles';
 
 const Home = () => {
   const [inputValue, setInputValue] = useState('');
 
   return (
-    <Container>
-      <Search>
-        <img src={logo} alt="App logo" />
-        <TextField label="Pesquisar" outlined>
-          <Input
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="App logo" />
+          <TextField
+            label="Pesquisar restaurantes"
+            outlined
             value={inputValue}
-            onChange={evt => setInputValue(evt.target.value)}
-          />
-        </TextField>
-      </Search>
-    </Container>
+            trailingIcon={<MaterialIcon role="button" icon="search" />}
+          >
+            <Input onChange={evt => setInputValue(evt.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
   );
 };
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+export const Wrapper = styled.div`
+  display: flex;
+`
+
 export const Container = styled.aside`
   background-color: ${(props) => props.theme.colors.background};
   width: 360px;
@@ -13,5 +17,13 @@ export const Search = styled.section`
   justify-content: center;
   background-color: #fff;
   padding: 16px;
+`
 
+export const Logo = style.img`
+  margin-bottom: 16px;
+`
+
+export const Map = styled.div`
+  background-color: blueviolet;
+  width: 500px;
 `
